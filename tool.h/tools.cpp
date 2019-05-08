cc04dd795fd3f7f2d8468dba29b9f8642852a428
jutyy <jutyy@163.com>
2019-05-08 22:10:37 +0800

    加入字符 "fa000001" 转换成16进字符串

--- tool.h/tools.cpp
+++ tool.h/tools.cpp
@@ -72,7 +72,28 @@ std::string hexStringToUnsignedInt(const std::string &hex)
 	intda = dest[3]; intda <<= 8; intda |= dest[2]; intda <<= 8; intda |= dest[1]; intda <<= 8; intda |= dest[0];
 	return to_string(intda);
 }
-
+int hexCharToInt(char c)
+{
+	if (c >= '0' && c <= '9') return (c - '0');
+	if (c >= 'A' && c <= 'F') return (c - 'A' + 10);
+	if (c >= 'a' && c <= 'f') return (c - 'a' + 10);
+	return 0;
+}
+string bytestohexstring(const char* bytes, int bytelength)
+{
+	string str("");
+	string str2("0123456789abcdef");
+	for (int i = 0; i<bytelength; i++) {
+		int b;
+		b = 0x0f & (bytes[i] >> 4);
+		char s1 = str2.at(b);
+		str.append(1, str2.at(b));
+		b = 0x0f & bytes[i];
+		str.append(1, str2.at(b));
+		char s2 = str2.at(b);
+	}
+	return str;
+}
 int main()
 {
 	std::string iccard1 = "87FA368D"; //2369190535
@@ -92,7 +113,22 @@ int main()
 
 	if (sintda == intcard1)printf("\n ok");
 	//int intda = dat[0] << 24 + dat[1] << 16 + dat[2] << 8 + dat[3];
-	printf("\n intdat =%d,%s", intda, sintda);
+	//printf("\n intdat =%d,%s", intda, sintda);
+
+	string aidstring = "fa000203";
+	char datchar[10]; datchar[aidstring.length()] = 0;
+	strcpy_s(datchar, aidstring.c_str());
+
+	
+	string stcda= bytestohexstring(datchar,8);
+	
+	cout <<"\r\n----"<< stcda<<"----dddddd";
+	  stcda = bytestohexstring(aidstring.c_str(), 8);  //½«¡®fa000203¡¯×ª»»³É¡¯66613030323033¡®
+
+	cout << "\r\n----" << stcda << "----dddddd";
+	//string sintdd = (datchar);
+
+
 	while (1)
 	{
 
